8b132de777d7afd36c0f929b4e5312e7e8a418c7
Joshua Yoes <>
2020-01-15 21:53:20 -0800

    Update default value

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,7 +11,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  { displayName: "something" },
+  { displayName: "Press a key" },
   composeEnhancers()
 );
 
